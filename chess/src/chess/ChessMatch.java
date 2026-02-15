48b9afcd11ac9235bebeae1c2acf7ed52726b1ff
thelastvascaino <lincolncdelfino@gmail.com>
2026-02-15 15:26:17 -0300

    Add ANSI colors to pieces

--- chess/src/chess/ChessMatch.java
+++ chess/src/chess/ChessMatch.java
@@ -16,8 +16,8 @@ public class ChessMatch {
 
     public ChessPiece[][] getPieces() {
         ChessPiece[][] matPiece = new ChessPiece[board.getRows()][board.getColumns()];
-        for(int i = 0; i < board.getRows(); i++) {
-            for(int j = 0; j < board.getColumns(); j++) {
+        for (int i = 0; i < board.getRows(); i++) {
+            for (int j = 0; j < board.getColumns(); j++) {
                 matPiece[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
@@ -25,9 +25,29 @@ public class ChessMatch {
     }
 
     private void initialSetup() {
-        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
-        placeNewPiece('e', 8, new King(board, Color.BLACK));
-        placeNewPiece('e', 1, new King(board, Color.WHITE));
+        // Teste com peças BRANCAS (vão aparecer em BRANCO - ANSI_WHITE)
+        // Posicionando reis e torres brancas
+        placeNewPiece('e', 1, new King(board, Color.WHITE)); // Rei branco em e1
+
+        placeNewPiece('a', 1, new Rook(board, Color.WHITE)); // Torres brancas
+        placeNewPiece('h', 1, new Rook(board, Color.WHITE));
+
+        // Algumas torres extras para testar
+        placeNewPiece('c', 1, new Rook(board, Color.WHITE)); // Torre extra em c1
+        placeNewPiece('f', 1, new Rook(board, Color.WHITE)); // Torre extra em f1
+
+        // Teste com peças PRETAS (vão aparecer em AMARELO - ANSI_YELLOW)
+        placeNewPiece('e', 8, new King(board, Color.BLACK)); // Rei preto em e8
+
+        placeNewPiece('a', 8, new Rook(board, Color.BLACK)); // Torres pretas
+        placeNewPiece('h', 8, new Rook(board, Color.BLACK));
+
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK)); // Torres extras
+        placeNewPiece('f', 8, new Rook(board, Color.BLACK));
+
+        // Colocar algumas peças no meio do tabuleiro para testar
+        placeNewPiece('d', 4, new Rook(board, Color.WHITE)); // Torre branca no centro
+        placeNewPiece('e', 5, new Rook(board, Color.BLACK)); // Torre preta no centro
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
