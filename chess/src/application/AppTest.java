7051d7de88239c1d30e1583e6181077c6d1bf7df
thelastvascaino <lincolncdelfino@gmail.com>
2026-02-16 18:09:12 -0300

    Temporary: implement possible moves structure

---
+++ chess/src/application/AppTest.java
@@ -0,0 +1,48 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+import chess.exception.ChessException;
+import util.ClearScreen;
+import util.Spause;
+import util.UI;
+
+public class AppTest {
+    public static void main(String[] args) {
+        ClearScreen.cls();
+        Scanner sc = new Scanner(System.in);
+        ChessMatch chessMatch = new ChessMatch();
+
+        while (true) {
+            try {
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                ClearScreen.cls();
+            }
+            catch(ChessException e) {
+                System.out.println("Error: " + e.getMessage() + " Try again!");
+                Spause.waitAnyKey();
+                sc.nextLine();
+                ClearScreen.cls();
+            }
+            catch(InputMismatchException e) {
+                System.out.println(e.getMessage() + " Try again!");
+                Spause.waitAnyKey();
+                sc.nextLine();
+                ClearScreen.cls();
+            }
+        }
+    }
+}
--- chess/src/boardgame/Piece.java
+++ chess/src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
     protected Position position;
     private Board board;
 
@@ -14,5 +14,23 @@ public class Piece {
 
     protected Board getBoard() {
         return board;
+    }
+
+    public abstract boolean[][] possibleMoves();
+
+    public boolean possibleMove(Position position) {
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    public boolean isThereAnyPossibleMove() {
+        boolean[][] matAux = possibleMoves();
+        for(int i = 0; i < matAux.length; i++) {
+            for(int j = 0; j < matAux.length; j++) {
+                if (matAux[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
